bbdc5b1747f806b16988bf093055b0d547502dbf
pav_nk <pav.nk.vh@gmail.com>
2024-10-20 03:08:02 +0300

    feat: connecting the Yandex maps API and adding the location detection function by IP

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import ymaps from 'ymaps';
 import {validateIp} from './helpers';
 
 const ipInput = document.querySelector('.search-bar__input');
@@ -7,19 +8,26 @@ const ipInfo = document.getElementById('ip');
 const locationInfo = document.getElementById('location');
 const timezoneInfo = document.getElementById('timezone');
 const ispInfo = document.getElementById('isp');
+const mapArea = document.getElementById('map');
 
-const API_KEY = 'https://geo.ipify.org/api/v2/country?apiKey=at_KzMEONg50rlnuBwgJWZQkxLf8GbO1&ipAddress=';
+const API_KEY = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_KzMEONg50rlnuBwgJWZQkxLf8GbO1&ipAddress=';
+
+let newMap = null;
 
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
 
 function setInfo(data) {
+    ipInput.value = '';
     const {ip, location, isp} = data;
-    const {country, region, timezone} = location;
+    const {country, region, timezone, lat, lng} = location;
+    console.log(newMap);
     ipInfo.innerHTML = ip;
     locationInfo.innerText = `${country} ${region}`;
     timezoneInfo.innerText = ` ${timezone}`;
     ispInfo.innerText = isp;
+
+    newMap.setCenter([lat, lng]);
 }
 
 function getData() {
@@ -33,6 +41,15 @@ function handleKey(evt) {
         getData();
     }
 }
+
+ymaps
+    .load('https://api-maps.yandex.ru/2.1/?apikey=c563d004-13c2-424e-9dbb-84a8050241a3&lang=ru_RU')
+    .then(maps => {
+        newMap = new maps.Map(mapArea, {
+        center: [-8.369326, 115.166023],
+        zoom: 10
+    });
+}).catch(error => console.log('Failed to load Yandex Maps', error));
 
 
 
